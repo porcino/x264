331cc8e077eac5c075ca25a1fd8797bc98900793
porcino <58743577+porcino@users.noreply.github.com>
2020-09-09 17:27:34 -0300

    aq-psy added

--- common/base.c
+++ common/base.c
@@ -413,6 +413,7 @@ REALIGN_STACK void x264_param_default( x264_param_t *param )
     param->rc.f_pb_factor = 1.3;
     param->rc.i_aq_mode = X264_AQ_VARIANCE;
     param->rc.f_aq_strength = 1.0;
+    param->rc.f_aq_psy = 1.0;
     param->rc.f_aq_dark = 1.0;
     param->rc.f_aq_dark_adapt = 0;
     param->rc.f_pb_dark = 1.0;
@@ -1317,6 +1318,8 @@ REALIGN_STACK int x264_param_parse( x264_param_t *p, const char *name, const cha
         p->rc.i_aq_mode = atoi(value);
     OPT("aq-strength")
         p->rc.f_aq_strength = atof(value);
+    OPT("aq-psy")
+        p->rc.f_aq_psy = atof(value);
     OPT("aq-dark")
         p->rc.f_aq_dark = atof(value);
     OPT("aq-dark-adapt")
@@ -1529,6 +1532,7 @@ char *x264_param2string( x264_param_t *p, int b_res )
             s += sprintf( s, ":%.2f", p->rc.f_aq_strength );
         if( p->rc.i_aq_mode > 2 )
         {
+            s += sprintf( s, " aq-psy=%.2f", p->rc.f_aq_psy );
             s += sprintf( s, " aq-dark=%.2f", p->rc.f_aq_dark );
             s += sprintf( s, " aq-dark-adapt=%.2f", p->rc.f_aq_dark_adapt );
             s += sprintf( s, " pb-dark=%.2f", p->rc.f_pb_dark );
